88dbeb14e792ca4176570bb4e3356a76e4d2f78f
Alain Adler <alain.adler@gmail.com>
2014-09-26 19:27:14 +0000

    notifier 1

---
+++ scripts/notifier.js
@@ -0,0 +1,13 @@
+// Description:
+//   notify all the things!
+//
+// URLS:
+//   POST /hubot/notify ({message:<message>, channel:<channel>, phones:[<phone1, phone2]})
+
+module.exports = function(robot){
+    robot.router.post('/hubot/notify', function(req, res){
+        var body = JSON.parse(req.body);
+        robot.messageRoom(body.channel, body.message);
+        res.end();
+    });
+}
